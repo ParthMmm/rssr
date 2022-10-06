ff35d329c99bcfe621f844ca77f35e1012e146ef
ParthMmm <parthmangrola@gmail.com>
2022-10-05 18:31:57 -0700

    cleaned up code

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,19 +1,7 @@
 import create from 'zustand';
 import { persist } from 'zustand/middleware';
 
-interface AppState {
-  filter: string;
-  setFilter: (filter: string) => void;
-}
-
-export const useAppStore = create<AppState>((set) => ({
-  filter: 'greentech',
-  setFilter: (filter: string) => set({ filter }),
-}));
-
 interface AppPersistState {
-  //   currentProfile: Profile | null;
-  //   setCurrentProfile: (currentProfile: Profile | null) => void;
   bookmarks: string[];
   addBookmark: (title: string) => void;
   removeBookmark: (title: string) => void;
